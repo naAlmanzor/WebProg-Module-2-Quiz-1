fbd9210752d82609d43fc099386522afbefb6bed
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-04 18:56:20 +0800

    Added quote variables

---
+++ app.js
@@ -0,0 +1,39 @@
+var felixQuotes = [
+    "You can just be you.",
+    "Haters gonna hate. Ducks gonna duck quack.",
+    "Everyone has played video games at some point these days, and video games are fun.",
+    "Life is too short to focus on the things you hate, focus on what you love instead.",
+    "Don\'t be a salad. Be the best damn broccoli you can ever be."
+]
+
+var markQuotes = [
+    "You have to start by believing in yourself",
+    "Life is hard, should you be too?",
+    "Sometimes you must reach beyond yourself to reach yourself within...yourself.",
+    "Not all those who wander are lost.",
+    "Sometimes, you just have to follow your own path."
+]
+
+var andrewQuotes = [
+    "How to be brilliant: Make something awesome and blame it on your brain.",
+    "I've gotta believe in what my heart tells me, even if it\'s ",
+    "You understand reality while everyone else is running arouond confused and angry and upset because they think reality is something happening to them rather than something they are making every momment with every thought",
+    "Don\'t even bother trying to look at your stat tracker. Focus on making something good",
+    "Maybe what I\'m discovering is people actually like complexity, and will even gravitate toward it, as long as it isn\'t a total train wreck"
+]
+
+var karinaQuotes = [
+    "Are we human? Or are we Luigi?",
+    "Just delete your art....It works everytime",
+    "It\'s not that I don't like it, I just...hate it.",
+    "That\'s art baby!",
+    "I\'m always looking for an excuse to draw a Yu-Gi-Oh! Character"
+]
+
+var edgarQuotes = [
+    "Sometimes, some things have to settle, and you have to think about the intention of it.",
+    "I think it's good to have pressure on yourself. The worst crime is to get kind of really complacent.",
+    "I use music to focus, like an internal motor.",
+    "I\'d rather try and cram in another two gags than leave a pause to say, \'Hey, wasn't that bit funny?\'",
+    "I am always watching old films and trying to fill gaps in my knowledge."
+]
